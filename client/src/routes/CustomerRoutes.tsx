8dece3691e9d77059f611b33b810503436b55dc9
antonisoaho <isoahoanton@gmail.com>
2024-02-12 12:23:02 +0100

    removing customerprint for now

--- client/src/routes/CustomerRoutes.tsx
+++ client/src/routes/CustomerRoutes.tsx
@@ -1,12 +1,10 @@
 import { Route } from 'react-router-dom';
 import CustomerComponent from '../components/customers';
-import CustomerPrintversion from '../components/customers/print/CustomerPrintversion';
 import CustomerView from '../components/customers/CustomerView';
 
 const CustomerRoutes = [
   <Route key="customers" path="/customers" element={<CustomerComponent />} />,
   <Route key="view" path="/customers/:custId" element={<CustomerView />} />,
-  <Route key="print" path="/customers/:custId/print" element={<CustomerPrintversion />} />,
 ];
 
 export default CustomerRoutes;
